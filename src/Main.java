0aad309d0226dffa62b22134a41bac61f69a9091
Vovashprot <vova.protasenko@gmail.com>
2023-04-07 15:22:29 +0300

    almost

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
+
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Hufflepuff adam = new Hufflepuff("Adam",65,63,61,63,63);
+        Hufflepuff adama = new Hufflepuff("Adama",33,23,35,42,62);
+        adama.hufflepuffComparison(adam);
     }
 }
